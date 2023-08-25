d04999c2549fa4f1cba8921fda2b61090fc5ad12
Hann <hannsolo7788@gmail.com>
2023-08-25 09:57:43 +0800

    Commited angpao-rain build.

---
+++ assets/resources/index.0b504.js
@@ -0,0 +1,20 @@
+System.register("chunks:///resources.js",["cc"],(function(){"use strict";var c;return{setters:[function(e){c=e.cclegacy}],execute:function(){c._RF.push({},"358b0aEgGJNQI8hT3SK4p6R","en-us",void 0),c._RF.pop(),c._RF.push({},"b370cCmtqJKq4hYOHb7C3Sq","zh-cn",void 0),c._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/resources', 'chunks:///resources.js'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
