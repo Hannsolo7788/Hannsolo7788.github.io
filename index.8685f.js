e55f4cf4e39815244569b011cc31aeafe34baf4b
Hann <hannsolo7788@gmail.com>
2023-09-01 00:12:59 +0800

    Created new build.

--- index.8685f.js
+++ index.8685f.js
@@ -1,4 +1,4 @@
-System.register(["./application.842bb.js"], function (_export, _context) {
+System.register(["./application.2c83a.js"], function (_export, _context) {
   "use strict";
 
   var createApplication, canvas, $p, bcr;
@@ -67,8 +67,8 @@ System.register(["./application.842bb.js"], function (_export, _context) {
   }
 
   return {
-    setters: [function (_application842bbJs) {
-      createApplication = _application842bbJs.createApplication;
+    setters: [function (_application2c83aJs) {
+      createApplication = _application2c83aJs.createApplication;
     }],
     execute: function () {
       canvas = document.getElementById('GameCanvas');
